5dfe405afc04e8ce8f0966572b2fc8f8cfb85f15
Ksusha <xiuschv@yandex.ru>
2023-04-11 18:20:36 +0600

    make look nicer

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
 const form = document.getElementById("task-form"); //получили форму
 const listContainer = document.getElementById("list-container"); //получили ul куда будут добавляться задачи
 
+const deleteAllChecked = document.querySelector(".deleteAllChecked");
+const deleteAll = document.querySelector(".deleteAll");
+
+
+
 //получаем из локалсторадж содержимое ul и записываем в переменную
 let taskCollector = localStorage.getItem("listContainer");
 //создаем пустой массив, куда будем добавлять объекты задач
@@ -8,23 +13,28 @@ let taskList = [];
 //если область задачи не пустая, то делаем из строкового представления данных в локалстораж нормальный массив с объектами
 if (taskCollector) taskList = JSON.parse(taskCollector);
 
+ 
+
 //функция для отрисовки задач
 function createNewTask(obj) {
+
   listContainer.style.display = "block";
   document.getElementById("button-wrapper").style.display = "flex";
-
+ 
   //создаем текстовое содержимое задачи
   let itemTask = document.createElement("li");
   let itemTaskText = document.createElement("p");
   itemTaskText.textContent = obj.taskValue;
   itemTask.classList.add("item");
   itemTask.append(itemTaskText);
+  if (obj.status) itemTaskText.classList.add("checked")
 
   //создаем checkbox
   let taskCheck = document.createElement("input");
   taskCheck.setAttribute("type", "checkbox");
   taskCheck.classList.add("checkbox");
   itemTask.append(taskCheck);
+  taskCheck.checked = obj.status;
   taskCheck.addEventListener("click", () => {
     itemTaskText.classList.toggle("checked"); //добавляем класс, чтобы при нажатии текст зачеркивался
 
@@ -46,25 +56,43 @@ function createNewTask(obj) {
   return itemTask;
 }
 
-// for (const task of taskList) {
-//   createNewTask(task);
-// }
 
-function renderTask(array) {
+
+function renderTask() {
   listContainer.innerHTML = '';
   // localStorage.setItem("listContainer", JSON.stringify(taskList));
-  array.forEach((el) => {
+  taskList.forEach((el) => {
     listContainer.append(createNewTask(el));
-    localStorage.setItem("listContainer", JSON.stringify(taskList));
   });
+  localStorage.setItem("listContainer", JSON.stringify(taskList));
 }
 
 renderTask(taskList)
 
 function deleteTask(id) {
-  taskList.filter((el) => el.id !== id);
+  taskList = taskList.filter((el) => el.id !== id);
   renderTask();
 }
+
+deleteAllChecked.addEventListener('click', () => {
+  taskList = taskList.filter(el => el.status !== true)
+  if (taskList.length == []) {
+      listContainer.style.display = "none";
+      document.getElementById("button-wrapper").style.display = "none";
+  }
+  renderTask()
+})
+
+
+deleteAll.addEventListener('click', () => {
+  listContainer.style.display = "none";
+  document.getElementById("button-wrapper").style.display = "none";
+  taskList = [];
+  renderTask();
+})
+
+
+
 
 //создаем функцию для создания уникального id задачи
 function getNewId(arr) {
@@ -87,98 +115,14 @@ form.addEventListener("submit", (evt) => {
     status: false,
   };
 
-  // createNewTask(taskObj);
 
   //добавляем объект с задачей в массив
   taskList.push(taskObj);
 
-  renderTask(taskList);
+  renderTask();
 
-  //записывваем с локалстораж массив объектов задач
-  // localStorage.setItem("listContainer", JSON.stringify(taskList));
 
   //после добавления задачи в лист поле ввода будет очищаться
   document.getElementById("inputTask").value = "";
 });
 
-// let input = document.getElementById("inputTask").value;
-
-// function createrLi(obj) {
-
-//   let itemTask = document.createElement("li");
-//   itemTask.classList.add("item");
-
-//   itemTask.innerHTML = `<input type="checkbox" id="itemCheck">
-//                         <p>${obj.value}</p>
-//                         <button id="deleteButton">❌</button>`;
-
-// }
-
-// document.getElementById('task-form').addEventListener("submit", (evt) => {
-//   evt.preventDefault();
-// })
-
-// let someObj = {
-//   id: (0.5 - Math.random),
-//   taskValue: 'то что введено в input',
-//   status: false
-// }
-
-// const listContainer = document.getElementById("list-container")
-
-// function drawTaskItem(obj) {
-//   let
-
-//   taskItem = document.createElement("li");
-//   listContainer.append(taskItem);
-
-// }
-
-// const form = document.querySelector("form");
-
-// const inputBox = document.getElementById("inputTask");
-// const listContainer = document.getElementById("list-container");
-
-// const addButton = document.getElementById("addTask");
-
-// addButton.addEventListener('click', addTask1)
-
-// function addTask1(e) {
-//   if (inputBox.value == "") {
-//     alert ('write smg')
-//   }
-//   else {
-//     listContainer.style.display = "block"
-//     let li = document.createElement("li");
-//     li.innerHTML = inputBox.value;
-//     listContainer.appendChild(li);
-
-//     let deleteLogo = document.createElement("span");
-//     deleteLogo.innerHTML = "❌";
-//     li.appendChild(deleteLogo)
-//   }
-
-//   inputBox.value = ""
-// }
-
-// listContainer.addEventListener("click", (evt) => {
-//   if (evt.target.tagName == "") {
-//     evt.target.classList.toggle("checked");
-//   }
-//   else if (evt.target.tagName == "span") {
-//     evt.target.parentElement.remove();
-//   }
-// }, false)
-
-// const addTask = (evt) => {
-//   evt.preventDefault(); //при нажатии на кнопку в форме страница обновляться не будет
-
-//   // let taskText = document.getElementById("inputTask").value;
-//   // console.log(taskText);
-
-//   // const itemTask = document.createElement("li")
-
-//   // const checkTask = document.create
-// }
-
-// form.addEventListener('submit', addTask)
